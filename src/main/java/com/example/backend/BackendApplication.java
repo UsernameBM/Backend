c616e44fec6522c5c87881344e1f336ffc27d984
Killian Walsh <Killian.walsh123@hotmail.com>
2022-04-05 15:53:08 +0200

    Screening and Salon classes

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -1,12 +1,16 @@
 package com.example.backend;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 
 public class BackendApplication {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
 
     public static void main(String[] args) {SpringApplication.run(BackendApplication.class, args);}
