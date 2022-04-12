ae043dc862e62b0cc471bce491e73aed1186d424
Henning Oksa <henning.oksa@gmail.com>
2022-04-12 19:40:17 +0200

    PersonalSchedule

--- src/main/java/com/example/backend/PersonalSchedule/PersonalSchedule.java
+++ src/main/java/com/example/backend/PersonalSchedule/PersonalSchedule.java
@@ -1,18 +1,28 @@
 package com.example.backend.PersonalSchedule;
 
 public class PersonalSchedule {
+    private int id;
     private String date;
     private int idUser;
     private int idSalon;
     private String Counter;
     private String Kitchen;
 
-    public PersonalSchedule(String date, int idUser, int idSalon, String Counter, String kitchen) {
+    public PersonalSchedule(int id, String date, int idUser, int idSalon, String Counter, String kitchen) {
+        this.id = id;
         this.date = date;
         this.idUser = idUser;
         this.idSalon = idSalon;
         this.Counter = Counter;
-        Kitchen = kitchen;
+        this.Kitchen = kitchen;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getDate() {
