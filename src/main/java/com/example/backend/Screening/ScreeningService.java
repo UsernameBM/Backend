f115c5d528e85ea3b2bf1775f2278605f4016d0b
bryan <bryan.bautista830@gmail.com>
2022-04-13 10:45:03 +0200

    Adderat flera metoder:)

--- src/main/java/com/example/backend/Screening/ScreeningService.java
+++ src/main/java/com/example/backend/Screening/ScreeningService.java
@@ -4,11 +4,14 @@ package com.example.backend.Screening;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class ScreeningService {
 
     @Autowired
     ScreeningDao screeningDao;
+    ArrayList<Screening> screenings;
 
     public String insertScreening(Screening screening){
         String salonAdded = "Your Salon has been added to the database with following Salon information: ";
@@ -19,5 +22,10 @@ public class ScreeningService {
     public Screening getScreeningById(String id){
         Screening screening = screeningDao.getScreeningById(id);
                 return screening;
+    }
+
+    public ArrayList<Screening> getAllScreening(){
+        screenings = screeningDao.getAllScreening();
+        return screenings;
     }
 }
